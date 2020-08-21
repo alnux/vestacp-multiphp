4e8db179702a78f8e5d40d2ce9a33013713941ce
Leonardo Allende Pasten <alnux@users.noreply.github.com>
2020-08-20 23:54:35 -0400

    centos 7 vestacp multiphp
    php files for vestacp multiphp

--- PHP-FPM-73.sh
+++ PHP-FPM-73.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 # Adding php pool conf
 user="$1"
@@ -9,7 +8,7 @@ docroot="$5"
 
 pool_conf="[$2]
 
-listen = /run/php56-fpm-$2.sock
+listen = /run/php73-fpm-$2.sock
 listen.owner = $1
 listen.group = $1
 listen.mode = 0666
@@ -26,7 +25,7 @@ pm.status_path = /status
 
 php_admin_value[upload_tmp_dir] = /home/$1/tmp
 php_admin_value[session.save_path] = /home/$1/tmp
-php_admin_value[open_basedir] = $5:/home/$1/tmp:/bin:/usr/bin:/usr/local/bin:/var/www/html:/tmp:/usr/share:/etc/phpmyadmin:/var/lib/phpmyadmin:/etc/roundcube:/var/log/round$
+php_admin_value[open_basedir] = $5:/home/$1/tmp:/bin:/usr/bin:/usr/local/bin:/var/www/html:/tmp:/usr/share:/etc/phpmyadmin:/var/lib/phpmyadmin:/etc/roundcube:/var/log/roundcube:/var/lib/roundcube
 php_admin_value[upload_max_filesize] = 80M
 php_admin_value[max_execution_time] = 20
 php_admin_value[post_max_size] = 80M
@@ -40,34 +39,24 @@ env[TMP] = /home/$1/tmp
 env[TMPDIR] = /home/$1/tmp
 env[TEMP] = /home/$1/tmp
 "
-pool_file_56="/opt/remi/php56/root/etc/php-fpm.d/$2.conf"
-pool_file_70="/opt/remi/php70/root/etc/php-fpm.d/$2.conf"
-pool_file_71="/opt/remi/php71/root/etc/php-fpm.d/$2.conf"
-pool_file_72="/opt/remi/php72/root/etc/php-fpm.d/$2.conf"
-pool_file_73="/opt/remi/php73/root/etc/php-fpm.d/$2.conf"
-pool_file_74="/opt/remi/php74/root/etc/php-fpm.d/$2.conf"
 
-write_file=0
-if [ ! -f "$pool_file_56" ]; then
-  write_file=1
-else
-  user_count=$(grep -c "/home/$1/" $pool_file_56)
-  if [ $user_count -eq 0 ]; then
-    write_file=1
-  fi
-fi
-if [ $write_file -eq 1 ]; then
-    echo "$pool_conf" > $pool_file_56
+pool_file_56="/opt/remi/php56/root/etc/php-fpm.d/$2.conf"
+pool_file_70="/etc/opt/remi/php70/php-fpm.d/$2.conf""
+pool_file_71="/etc/opt/remi/php71/php-fpm.d/$2.conf"
+pool_file_72="/etc/opt/remi/php72/php-fpm.d/$2.conf"
+pool_file_73="/etc/opt/remi/php73/php-fpm.d/$2.conf"
+pool_file_74="/etc/opt/remi/php74/php-fpm.d/$2.conf"
+
+if [ -f "$pool_file_56" ]; then
+    rm $pool_file_56
     systemctl restart php56-php-fpm
-fi
-if [ -f "/opt/remi/php56/root/etc/php-fpm.d/www.conf" ]; then
-    rm /opt/remi/php56/root/etc/php-fpm.d//www.conf
 fi
 
 if [ -f "$pool_file_70" ]; then
     rm $pool_file_70
-     systemctl restart php70-php-fpm
+    systemctl restart php70-php-fpm
 fi
+
 if [ -f "$pool_file_71" ]; then
     rm $pool_file_71
     systemctl restart php71-php-fpm
@@ -78,9 +67,21 @@ if [ -f "$pool_file_72" ]; then
     systemctl restart php72-php-fpm
 fi
 
-if [ -f "$pool_file_73" ]; then
-    rm $pool_file_73
+write_file=0
+if [ ! -f "$pool_file_73" ]; then
+  write_file=1
+else
+  user_count=$(grep -c "/home/$1/" $pool_file_73)
+  if [ $user_count -eq 0 ]; then
+    write_file=1
+  fi
+fi
+if [ $write_file -eq 1 ]; then
+    echo "$pool_conf" > $pool_file_73
     systemctl restart php73-php-fpm
+fi
+if [ -f "/etc/opt/remi/php73/php-fpm.d/www.conf" ]; then
+    rm /etc/opt/remi/php73/php-fpm.d/www.conf
 fi
 
 if [ -f "$pool_file_74" ]; then
